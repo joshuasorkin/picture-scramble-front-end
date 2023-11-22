44dc1bc29e99d853af8bad28e4b704a03b710ebb
joshuasorkin <joshua.d.sorkin@gmail.com>
2023-11-21 22:09:48 -0800

    add state for isloading

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import GamePicture from './components/GamePicture/GamePicture';
 import CheckButton from './components/CheckButton/CheckButton';
 import PlayerInput from './components/PlayerInput/PlayerInput';
 import Scramble from './components/Scramble/Scramble';
+import React, {useState} from 'react';
 
 function App() {
+  const [isLoading,setIsLoading] = useState(false);
+  
   return(
     <>
       <GamePicture />
