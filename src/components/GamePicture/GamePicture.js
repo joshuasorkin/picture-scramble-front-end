4b051b91f0f15beaebf9a649e55e720c6109c4a7
joshuasorkin <joshua.d.sorkin@gmail.com>
2023-11-21 23:01:00 -0800

    implement spin

--- src/components/GamePicture/GamePicture.js
+++ src/components/GamePicture/GamePicture.js
@@ -1,5 +1,21 @@
 import React, {useState} from 'react';
 
-export default function GamePicture(){
-    
+export default function GamePicture({imageUrl}){
+    const [isSpinning,setIsSpinning] = useState(false);
+
+    const handleSpinClick = () => {
+        setIsSpinning(!isSpinning);
+    }
+
+    return (
+        <>
+            <button onClick={handleSpinClick}>
+                {isSpinning ? 'Stop spinning' : 'Spin 360°'}
+            </button>
+            <div
+                className={`game-picture ${isSpinning ? 'spin' : ''}`}
+                style={{backgroundImage: `url(${imageUrl})`}}
+            ></div>
+        </>
+    )
 }
