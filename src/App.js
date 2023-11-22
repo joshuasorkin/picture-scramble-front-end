253ff7212bb8eac09ad465e7ae5810a81836fd2a
joshuasorkin <joshua.d.sorkin@gmail.com>
2023-11-21 21:45:55 -0800

    fill in components in App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import Header from './components/Header/Header';
-import RegistrationForm from './components/RegistrationForm/RegistrationForm';
-import Chat from './components/Chat/Chat';
-import Home from './components/Home/Home';
-import DataDisplay from './components/DataDisplay/DataDisplay';
-import {Route,Routes} from 'react-router-dom';
+import GamePicture from './components/GamePicture/GamePicture';
+import CheckButton from './components/CheckButton/CheckButton';
+import PlayerInput from './components/PlayerInput/PlayerInput';
+import Scramble from './components/Scramble/Scramble';
 
 function App() {
   return(
     <>
-        <SocketContext.Provider value={socket}>
-          <Header />
-          <div className="container">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/register" element={<RegistrationForm />} />
-              <Route path="/chat" element={<Chat />} />
-              <Route path="/display" element={<DataDisplay />} />
-            </Routes>
-          </div>
-        </SocketContext.Provider>
+      <GamePicture />
+      <Scramble />
+      <PlayerInput />
+      <CheckButton />
     </>
   )
 }  
