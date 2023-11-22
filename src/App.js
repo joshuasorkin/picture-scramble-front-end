50c41db08c8eed88e0239e26cc1ec17cf50d8e5c
joshuasorkin <joshua.d.sorkin@gmail.com>
2023-11-21 22:19:34 -0800

    create stub function for checkInput

--- src/App.js
+++ src/App.js
@@ -4,9 +4,26 @@ import PlayerInput from './components/PlayerInput/PlayerInput';
 import Scramble from './components/Scramble/Scramble';
 import React, {useState} from 'react';
 
+
+function checkInput(inputValue){
+  return new Promise((resolve) => {
+    setTimeout(() => {
+        resolve({
+          status:'success',
+          message: `Input value '${inputValue}' checked.`
+        },2000);
+      }
+    )
+  });
+}
+
 function App() {
   const [isLoading,setIsLoading] = useState(false);
-  
+  const handleCheckButtonClick = (inputValue) => {
+    setIsLoading(true);
+
+  }
+
   return(
     <>
       <GamePicture />
