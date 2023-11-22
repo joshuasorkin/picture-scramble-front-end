e8bbcc7db76c3e6190bcd6d8968cdf37393e2292
joshuasorkin <joshua.d.sorkin@gmail.com>
2023-11-21 22:49:38 -0800

    implement fetchgamedata, imageurl and scrambledword

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import GamePicture from './components/GamePicture/GamePicture';
 import CheckButton from './components/CheckButton/CheckButton';
 import PlayerInput from './components/PlayerInput/PlayerInput';
 import Scramble from './components/Scramble/Scramble';
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 
 
 function checkInput(inputValue){
@@ -17,10 +17,41 @@ function checkInput(inputValue){
   });
 }
 
+function fetchGameData(){
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({
+        scrambledWord:'xxyy',
+        imageUrl:'test.jpg'
+      });
+    },2000);
+  })
+}
+
 function App() {
   const [isLoading,setIsLoading] = useState(false);
+  const [scrambledWord,setScrambledWord] = useState('');
+  const [imageUrl,setImageUrl] = useState('');
   const [message, setMessage] = useState('');
 
+  useEffect(() => {
+    async function startNewGame() {
+      setIsLoading(true);
+      try{
+        const {scrambledWord, imageUrl} = await fetchGameData();
+        setScrambledWord(scrambledWord);
+        setImageUrl(imageUrl);
+        setMessage('');
+      }
+      catch (error){
+        console.error(error);
+        setMessage('Error occured.');
+      }
+      finally{
+        setIsLoading(false);
+      }
+    }
+  })
   const handleCheckButtonClick = async (inputValue) => {
     setIsLoading(true);
 
@@ -39,8 +70,8 @@ function App() {
 
   return(
     <>
-      <GamePicture />
-      <Scramble />
+      <GamePicture imageUrl={imageUrl} />
+      <Scramble scrambledWord={scrambledWord} />
       <PlayerInput />
       <CheckButton onClick={handleCheckButtonClick} disabled={isLoading} />
       <p>{message}</p>
